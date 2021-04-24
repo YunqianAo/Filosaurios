898cb6bbe0629c46c9e01ea1c4e28a15f7855a36
Luxary-92 <79161171+Luxary-92@users.noreply.github.com>
2021-04-24 20:47:29 +0200

    No funcional colaider modules

---
+++ Prototipe 2 Save Point/Super Smash TV/Source/Collider.cpp
@@ -0,0 +1,20 @@
+#include "Collider.h"
+
+Collider::Collider(SDL_Rect rectangle, Type type, Module* listener) : rect(rectangle), type(type), listener(listener)
+{
+
+}
+
+void Collider::SetPos(int x, int y)
+{
+	rect.x = x;
+	rect.y = y;
+}
+
+bool Collider::Intersects(const SDL_Rect& r) const
+{
+	// L6: TODO 1: Check if there is an overlap
+	// between argument "r" and property "rect"
+
+	return false;
+}
