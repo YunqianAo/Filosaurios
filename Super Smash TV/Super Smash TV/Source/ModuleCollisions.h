b17156d4b89454669fd2bc3f65e20d29e79e3174
Ludef26 <Ludef26@gmail.com>
2021-04-28 23:44:00 +0200

    Irrelevant Changes

--- Super Smash TV/Super Smash TV/Source/ModuleCollisions.h
+++ Super Smash TV/Super Smash TV/Source/ModuleCollisions.h
@@ -35,12 +35,11 @@ public:
 	// Adds a new collider to the list
 	Collider* AddCollider(SDL_Rect rect, Collider::Type type, Module* listener = nullptr);
 
-	// Draws all existing colliders with some transparency
-	void DebugDraw();
-	
 	// Removes the collider memory and removes it from the colliders array
 	void RemoveCollider(Collider* collider);
 
+	// Draws all existing colliders with some transparency
+	void DebugDraw();
 
 private:
 	// All existing colliders in the scene
